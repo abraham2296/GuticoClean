29b6aac47eb4dad4fc83bd9d0a23d1454db62390
Edgar Sanabria <abraham96abraham@gmail.com>
2024-01-12 11:35:12 -0400

    subiendo el proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+
+// interface carrusel{
+//   imgSrc: string;
+//   imgAlt: string;
+// }
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, NgbCarouselModule{
   title = 'GuticoClean';
+  // @Input() images: carrusel[] = [];
+  // selectedIndex = 0;
+
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+	constructor(){}
+  ngOnInit(): void {}
+
+ 
 }
