16443e033da7cf216954cf2e7f3467840dd67f46
Edgar Sanabria <abraham96abraham@gmail.com>
2024-01-15 16:34:52 -0400

    proyecto finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
-
-// interface carrusel{
-//   imgSrc: string;
-//   imgAlt: string;
-// }
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, NgbCarouselModule{
+export class AppComponent{
   title = 'GuticoClean';
-  // @Input() images: carrusel[] = [];
-  // selectedIndex = 0;
 
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
-
-	constructor(){}
-  ngOnInit(): void {}
 
  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
@@ -11,8 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule,
-    NgbCarouselModule
+    // NgbModule,
+    // NgbCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
